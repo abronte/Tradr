aa5c62202175c41a04f40ca1e357cc7e0bd6bdb9
Adam Bronte <abronte@gmail.com>
2011-08-12 14:42:04 -0700

    show green/red in prices paid

--- static/trade.js
+++ static/trade.js
@@ -3,3 +3,21 @@ function sell(ticker) {
 
 	});
 }
+
+$(function() {
+	$('tr').each(function() {
+		var bought_h = $(this).find("td:eq(4)");
+		var current_h = $(this).find("td:eq(6)");
+		var bought = parseFloat(bought_h.html());
+		var current = parseFloat(current_h.html());
+
+		if(bought != 0) {
+			if(bought > current) {
+				bought_h.css("background-color","red");
+			} else {
+				bought_h.css("background-color","green");
+			}
+		}
+
+	});
+});
