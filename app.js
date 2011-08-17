6723fbe8b798875b7040231b8540da57cd34d75b
Adam Bronte <abronte@gmail.com>
2011-08-17 12:38:29 -0700

    typo

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ process.on('uncaughtException', function(err) {
 });
 
 var watch = ['CRZO', 'JOYG', 'DDD', 'PIR', 'ABB', 'VNDA', 'BAC', 'AMD', 'F', 'MGM', 'NVDA', 'SD', 'HTZ', 'CAR', 'LVS'];
-var shares= {'CRZO': 60, 'JOYG':24, 'DDD': 100, 'PIR': 200, 'ABB': 50, 'VNDA': 200, 'BAC': 260, 'AMD':320, 'F':180, 'MGM':172, 'NVDA':148, 'SD':266, 'HTZ':184, 'CAR', 200, 'LVS':67};
+var shares= {'CRZO': 60, 'JOYG':24, 'DDD': 100, 'PIR': 200, 'ABB': 50, 'VNDA': 200, 'BAC': 260, 'AMD':320, 'F':180, 'MGM':172, 'NVDA':148, 'SD':266, 'HTZ':184, 'CAR': 200, 'LVS':67};
 var portfolio = {};
 var transactions = [];
 
